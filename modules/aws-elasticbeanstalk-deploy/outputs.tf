b7b40614766fb8927b332f41a4dce387a1ac431f
Cristian Beraha <cristian@cloudops.works>
2022-04-12 11:09:30 -0300

    Moved modules used in the process to Terraform Registry

--- modules/aws-elasticbeanstalk-deploy/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "environment_cname" {
-  value = aws_elastic_beanstalk_environment.beanstalk_environment.cname
-}
--- modules/aws-elasticbeanstalk-versions/beanstalk-app.tf
+++
@@ -1,8 +0,0 @@
-##
-# (c) 2021 - Cloud Ops Works LLC - https://cloudops.works/
-#            On GitHub: https://github.com/cloudopsworks
-#            Distributed Under Apache v2.0 License
-#
-data "aws_elastic_beanstalk_application" "application" {
-  name = var.beanstalk_application
-}
